cd5822147741b6b10d4e1362f4e087e5488f844a
Alyssa Blanco <alyssakblanco@outlook.com>
2022-11-03 23:15:06 -0600

    finished assignment

---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,37 @@
+﻿// based on CoinSpawnner.cs
+using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite gem spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnGems() {
+		while (true) {
+			Debug.Log("here");
+
+			// number of gems we could spawn vertically
+			int gemsThisRow = 1;
+
+			// instantiate all gems in this row separated by some random amount of space
+			for (int i = 0; i < gemsThisRow; i++) {
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+			}
+
+			// pause 10-25 seconds until the next gem spawns
+			yield return new WaitForSeconds(Random.Range(10, 25));
+		}
+	}
+}
